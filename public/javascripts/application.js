d76ff3b7204a33359cf39c09560ed39918186173
KATO TAKUYA <fanciful.dream@gmail.com>
2009-08-05 11:44:48 +0900

    Mod

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function(){
+  $(".corner_round").corner();
+  $(".corner_round_top").corner("top");
+  $(".corner_round_bottom").corner("bottom");
+
+  $("#show_new_meegen").click(function(){
+    $("#new_meegen").blindToggle();
+  });
+
+  //$("#tabs").tabs();
+});
+
+
