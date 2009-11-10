31ae3c17270f7d9312687deba9854a93a380511f
KATO TAKUYA <fanciful.dream@gmail.com>
2009-11-10 20:13:58 +0900

    add migrationn drop user and session

---
+++ db/migrate/20091109235419_drop_user_and_session.rb
@@ -0,0 +1,34 @@
+class DropUserAndSession < ActiveRecord::Migration
+  def self.up
+    drop_table :users
+    drop_table :sessions
+  end
+
+  def self.down
+    create_table "users", :force => true do |t|
+      t.column :login,                     :string, :limit => 40
+      t.column :name,                      :string, :limit => 100, :default => '', :null => true
+      t.column :email,                     :string, :limit => 100
+      t.column :crypted_password,          :string, :limit => 40
+      t.column :salt,                      :string, :limit => 40
+      t.column :created_at,                :datetime
+      t.column :updated_at,                :datetime
+      t.column :remember_token,            :string, :limit => 40
+      t.column :remember_token_expires_at, :datetime
+
+
+    end
+    add_index :users, :login, :unique => true
+
+    create_table :sessions do |t|
+      t.string :session_id, :null => false
+      t.text :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id
+    add_index :sessions, :updated_at
+
+
+  end
+end
